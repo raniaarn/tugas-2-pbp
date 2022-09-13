0053916410a505498b5880503586798951598aca
raniaarn <rania.maharani11@ui.ac.id>
2022-09-13 13:36:01 +0700

    add new configuration

--- project_django/settings.py
+++ project_django/settings.py
@@ -14,9 +14,12 @@ import os
 import dj_database_url
 from pathlib import Path
 
+#add configuration 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 PRODUCTION = os.getenv('DATABASE_URL') is not None
 
 # Quick-start development settings - unsuitable for production

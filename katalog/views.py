560d3f9c07bb9127b8df0857bf17b3a1eeb7a6aa
raniaarn <rania.maharani11@ui.ac.id>
2022-09-13 13:25:56 +0700

    create function, routing, and mapping

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_of_items': data_barang_katalog,
+        'name': 'Rania Maharani Narendra',
+        'student_id' : '2106650222'
+    }
+    return render(request, "katalog.html", context)

a1211fbacd56aa8ef0d59252e5e12f7125b72d41
raniaarn <rania.maharani11@ui.ac.id>
2022-09-20 23:01:31 +0700

    update procfile

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class TestUrls(TestCase):
+    def test_html_url(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_watchlist"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_json_url(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_json"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_xml_url(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_xml"))
+        self.assertEquals(response.status_code, 200)
